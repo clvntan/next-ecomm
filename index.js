8c528f6b179521efa9456b1e6260d47ab00375d1
Cal Tan <Calvin@Cals-MacBook-Pro.local>
2024-02-23 10:47:58 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import prisma from "./src/utils/prisma.js"
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
