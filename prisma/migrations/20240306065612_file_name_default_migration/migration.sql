35a8d7692bda46bf62d6d1420958003e03d5aa6f
Cal <calvintan.dou@gmail.com>
2024-03-06 18:07:56 +0800

    Updated Schema

---
+++ prisma/migrations/20240306065612_file_name_default_migration/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Image" ADD COLUMN     "fileName" TEXT NOT NULL DEFAULT 'default_filename';
---
+++ prisma/migrations/20240306065713_added_file_name_migration/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Image" ALTER COLUMN "fileName" DROP DEFAULT;
